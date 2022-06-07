9a133a59135d5a00712716eb837e4fa3680ee118
Cuong-lee32 <cuong.le322002@gmail.com>
2022-06-07 17:18:40 +0700

    fix proj

--- client/src/NavBar/NavBar.jsx
+++ client/src/NavBar/NavBar.jsx
@@ -4,16 +4,23 @@ import './NavBar.css'
 export default function NavBar() {
   return (
     <div className='top'>
-        <div className="top__menu">
-          <i class="fa-solid fa-bars-sort"></i>
+      <div className="top__menu">
+      <i class="fa-solid fa-bars"></i>
+      </div>
+      <div className="top__name">
+        <a href="#">TEKUN.IO</a>
         </div>
-        <div className="top__name">TEKUN.IO</div>
-        <div className="top__nav">
-          <i class="fa-solid fa-magnifying-glass"/>
-          <button className="top__nav-button">
-            login
-          </button>
-        </div>
+      <div className="top__nav">
+        <div class="content">
+            <div class="search">
+              <input type="text" class="search__input" aria-label="search" placeholder="enter your search"/>
+              <button class="search__submit" aria-label="submit search"><i class="fas fa-search"></i></button>
+            </div>
+          </div>
+        <button className="top__nav-button" type="button">
+          <p>Login</p>
+        </button>
+      </div>
     </div>
   )
 }
