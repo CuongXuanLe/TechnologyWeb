5e12d0ef1200b8456582ccdbdfe3cdaa67b5d35a
Cuong-lee32 <cuong.le322002@gmail.com>
2022-06-08 20:39:25 +0700

    add home_header

--- client/src/Components/Home/HomeHeader/HomeHeader.jsx
+++ client/src/Components/Home/HomeHeader/HomeHeader.jsx
@@ -10,29 +10,143 @@ const HomeHeader = () => {
           {/* main-article */}
           <div className="main__article">
             <img src={images.iphone} alt="iphone"/>
-              <div className="homeHeader__article-tittle">
-                <p className="article__tittle-header">
-                  Why are you gay?
-                </p>
-                <p className="article__tittle-sub">
-                Lorem ipsum dolor sit amet, consectetur 
-                adipiscing elit, sed do eiusmod tempor 
-                incididunt ut labore et dolore magna aliqua.
-                </p>
-                <div className="article__type">
-                  <p>TECH</p>
-                </div>
+            <div className="homeHeader__article-tittle">
+              <p className="article__tittle-header">
+                Why are you gay?
+              </p>
+              <p className="article__tittle-sub">
+              Lorem ipsum dolor sit amet, consectetur 
+              adipiscing elit, sed do eiusmod tempor 
+              incididunt ut labore et dolore magna aliqua.
+              </p>
+              <div className="article__type">
+                <p>TECH</p>
               </div>
+            </div>
           </div>
 
           {/* sub-article */}
           <div className="sub__article-col">
             <div className="sub__article-row">
-
+              <div className="sub__article-text">
+                <img src={images.content_2} alt="image2" />
+                <div className="header__sub-article">
+                <p className="sub__article-header">
+                  Why are you gay?
+                </p>
+                <p className="article__tittle-content">
+                Lorem ipsum dolor sit amet, consectetur 
+                adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua.
+                </p>
+                <div className="sub__article-type">
+                  <p>LIFE</p>
+                </div>
+              </div>
+            </div>
+            <div className="sub__article-text">
+                <img src={images.content_3} alt="image2" />
+                <div className="header__sub-article">
+                <p className="sub__article-header">
+                  Why are you gay?
+                </p>
+                <p className="article__tittle-content">
+                Lorem ipsum dolor sit amet, consectetur 
+                adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua.
+                </p>
+                <div className="sub__article-type">
+                  <p>TRUTH</p>
+                </div>
+              </div>
             </div>
           </div>
-        
+          <div className="sub__article-text bottom1">
+            <img src={images.content_4} alt="image2" />
+              <div className="header__sub-article">
+              <p className="sub__article-header">
+                Why are you gay?
+              </p>
+              <p className="article__tittle-content">
+              Lorem ipsum dolor sit amet, consectetur 
+              adipiscing elit, sed do eiusmod tempor 
+              incididunt ut labore et dolore magna aliqua.
+              </p>
+              <div className="sub__article-type">
+                <p>LIFE</p>
+              </div>
+            </div>
+          </div>
         </div>
+        </div>
+
+        <div className="sub__article-row">
+          <div className="sub__article-text">
+                <img src={images.content_5} alt="image2" />
+                <div className="header__sub-article">
+                <p className="sub__article-header">
+                  Yes, you're gay!
+                </p>
+                <p className="article__tittle-content">
+                Lorem ipsum dolor sit amet, consectetur 
+                adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua.
+                </p>
+                <div className="sub__article-type">
+                  <p>TRUTH</p>
+                </div>
+              </div>
+          </div>
+          <div className="sub__article-text">
+                <img src={images.content_6} alt="image2" />
+                <div className="header__sub-article">
+                <p className="sub__article-header">
+                  Yes, you're gay!
+                </p>
+                <p className="article__tittle-content">
+                Lorem ipsum dolor sit amet, consectetur 
+                adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua.
+                </p>
+                <div className="sub__article-type">
+                  <p>LIFE</p>
+                </div>
+              </div>
+          </div>
+          <div className="sub__article-text">
+                <img src={images.content_7} alt="image2" />
+                <div className="header__sub-article">
+                <p className="sub__article-header">
+                  Yes, you're gay!
+                </p>
+                <p className="article__tittle-content">
+                Lorem ipsum dolor sit amet, consectetur 
+                adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua.
+                </p>
+                <div className="sub__article-type">
+                  <p>TECH</p>
+                </div>
+              </div>
+          </div>
+          <div className="sub__article-text">
+            <img src={images.content_8} alt="image2" />
+            <div className="header__sub-article">
+            <p className="sub__article-header">
+              Yes, you're gay!
+            </p>
+            <p className="article__tittle-content">
+            Lorem ipsum dolor sit amet, consectetur 
+            adipiscing elit, sed do eiusmod tempor 
+            incididunt ut labore et dolore magna aliqua.
+            </p>
+            <div className="sub__article-type">
+              <p>TRUTH</p>
+            </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   )
