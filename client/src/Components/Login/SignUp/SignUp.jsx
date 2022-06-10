16ce3ae7b5398964ea473386482d2c65c4701b5b
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2022-06-11 01:47:13 +0700

    finnish SignUp

--- client/src/Components/Login/SignUp/SignUp.jsx
+++ client/src/Components/Login/SignUp/SignUp.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './SignUp.css'
+import images from '../../../constants/images';
 
 const SignUp = () => {
   return (
     <div className="SignUp__main">
-      
+      <div className="SignIn__img">
+        <img src={images.login} alt="Login_image" />
+      </div>
+      <div className="SignIn__info SignUp__fix">
+        <div className="SignIn__header">
+          <p>Hello</p>
+          <h1>Create your account</h1>
+        </div>
+        <form className="btn__input">
+          <input type="text" className="input__account" placeholder="Username"/>
+          <input type="email" className="input__account" placeholder="Email"/>
+          <input type="password" className="input__password" placeholder="Password"/>
+          <input type="password" className="input__password" placeholder="Re-enter password"/>
+          <div className="Privacy__Prolicy">
+            <p>I agree with <span>Terms of Service</span> and <span>Privacy Prolicy</span></p>
+          </div>
+          <div className="input__SignIn">
+            <Link to="/login">
+              <p>Sign Up</p>
+            </Link>
+          </div>
+        </form> 
+      </div>
     </div>
   )
 }
