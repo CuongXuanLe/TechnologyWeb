9cf346f318751ffe262670b3dd84e00ea4dad439
Cuong-lee32 <cuong.le322002@gmail.com>
2022-06-12 16:17:55 +0700

    finish change password page

--- client/src/Components/ChangePassword/ChangePassword.jsx
+++ client/src/Components/ChangePassword/ChangePassword.jsx
@@ -4,7 +4,13 @@ import './ChangePassword.css'
 const ChangePassword = () => {
   return (
     <div className='main__changePassword'>
-      
+      <h1>Change your password</h1>
+      <div className="input__changePassword">
+        <input type="password" placeholder='Old password...'/>
+        <input className='fix__m' type="password" placeholder='New password...'/>
+        <input type="password"  placeholder='Confirm new password...'/>
+      </div>
+      <button type='button' className='btn__submit'>Submit</button>
     </div>
   )
 }
