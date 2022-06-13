cef8f34ddd22fc3ca561e604c3392395d5eebdaa
Cuong-lee32 <cuong.le322002@gmail.com>
2022-06-13 10:34:55 +0700

    finish profile page

--- client/src/Components/Profile/ProfilePage.jsx
+++ client/src/Components/Profile/ProfilePage.jsx
@@ -25,21 +25,49 @@ const ProfilePage = () => {
         </div>
       </div>
       <div className="profile__recent-active">
-        <div className="profile__active-title">
-          <p>Recent Activity</p>
-        </div>
-        <div className="comment__info">
-            <div className="comment__info-user">
-                <img src={images.ProfilePicture} alt="user" />
-                <p className="user__name">MikeJoester</p>
-                <p className="user__reply">May 12, 2022 at 17:06</p>
-            </div>
-        </div>
-        <div className="active_comment">
+        <p className='recent__active-tittle'>Recent activity</p>
+        <div className="recent__active">
+          <div className="recent__active-info">
+            <img src={images.avatarLarge} alt="" />
+            <p><span>MikeJoseter</span> May 22, 2022 at 23:17</p>
+          </div>
           <p>
-          Basically what you want to do is delete the file containing the logs which is your ".git" file.<br/>
-            1. rm -rf .git // this is saying remove recursively the git file.<br/>
-            2. git init // this is making the folder a git repo again.<br/>
+            Basically what you want to do is delete the file containing the logs which is your ".git" file.<br/>
+            <span className='pl-10px'>
+              1. rm -rf .git // this is saying remove recursively the git file.<br/>
+              2. git init // this is making the folder a git repo again.<br/>
+            </span>
+          </p>
+        </div>
+        <div className="recent__active">
+          <div className="recent__active-info">
+            <img src={images.avatarLarge} alt="" />
+            <p><span>MikeJoseter</span> May 22, 2022 at 23:17</p>
+          </div>
+          <p>
+          The protocol necessary depends on how you're connecting to git. The example provided assumes you are using the git protocol. The git book explains various protocols supported by git.
+          </p>
+        </div>
+        <div className="recent__active">
+          <div className="recent__active-info">
+            <img src={images.avatarLarge} alt="" />
+            <p><span>MikeJoseter</span> May 22, 2022 at 23:17</p>
+          </div>
+          <p>
+            Basically what you want to do is delete the file containing the logs which is your ".git" file.<br/>
+            <span className='pl-10px'>
+              1. rm -rf .git // this is saying remove recursively the git file.<br/>
+              2. git init // this is making the folder a git repo again.<br/>
+            </span>
+          </p>
+        </div>
+        <div className="recent__active">
+          <div className="recent__active-info">
+            <img src={images.avatarLarge} alt="" />
+            <p><span>MikeJoseter</span> May 22, 2022 at 23:17</p>
+          </div>
+          <p>
+          The protocol necessary depends on how you're connecting to git. The example provided assumes you are using the git protocol. The git book explains various protocols supported by git.
           </p>
         </div>
       </div>
