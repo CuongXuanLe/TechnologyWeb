2395d66e3dad78506ac540e291bf659195eb1648
Cuong-lee32 <cuong.le322002@gmail.com>
2022-06-16 17:20:48 +0700

    fix auth

--- api/index.js
+++ api/index.js
@@ -10,21 +10,23 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 
-// mongoose.connect(process.env.MONGO_URL)
-//     .then(console.log("Connected to MongoDB"))
-//     .catch((err) => console.log(err));
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(console.log("Connected to MongoDB"))
+    .catch((err) => console.log(err));
 
 
-try{
-    mongoose.connect(process.env.MONGO_URL);
-    console.log("Connected to MongoDB");
-} catch(err) {
-    console.log(err);
-    console.log("Couldn't connect to MongoDB");
-}
+// try{
+//     mongoose.connect(process.env.MONGO_URL);
+//     console.log("Connected to MongoDB");
+// } catch(err) {
+//     console.log(err);
+//     console.log("Couldn't connect to MongoDB");
+// }
 
 
 app.use("/api/auth", authRoute);
-app.listen(5000, () => {
+
+app.listen("5000", () => {
     console.log("Backend is running.");
 })
