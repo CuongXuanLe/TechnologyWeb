574ffc8ad7e4018b492e697a43e66b9a6f99db14
Cuong-lee32 <cuong.le322002@gmail.com>
2022-06-16 20:48:35 +0700

    add images

--- api/index.js
+++ api/index.js
@@ -18,6 +18,21 @@ mongoose
     .then(console.log("Connected to MongoDB"))
     .catch((err) => console.log(err));
 
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    },
+});
+    
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+});
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
